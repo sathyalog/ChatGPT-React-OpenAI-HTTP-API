3ddbf04eda90d58da662c75a263cbe558b213c2e
Sathya Vakacharla <sathyavakacharla@192.168.1.107>
2023-03-13 18:00:27 +0000

    removing key

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const fetchData = async (input: string) => {
-  const API_KEY = "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";
   const model = "text-davinci-002";
   const response = await axios.post(
     "https://api.openai.com/v1/completions",
